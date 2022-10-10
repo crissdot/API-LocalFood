d44fa76ce83c0da23318e23abc3197078cd3e1a0
crissdot <crissdotdev@gmail.com>
2022-10-09 21:59:39 -0500

    add localfood detail

--- apps/localfood/api/api.py
+++ apps/localfood/api/api.py
@@ -1,10 +1,10 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from ..models import LocalFood
 from .serializers import LocalFoodSerializer
 
 class LocalFoodAPIView(APIView):
-
   def get(self, request):
     local_food = LocalFood.objects.all()
     local_food_serializer = LocalFoodSerializer(local_food, many = True)
@@ -16,3 +16,28 @@ class LocalFoodAPIView(APIView):
       local_food_serializer.save()
       return Response(local_food_serializer.data)
     return Response(local_food_serializer.errors)
+
+class LocalFoodDetailAPIView(APIView):
+  def get_localfood(self, pk):
+    try:
+      return LocalFood.objects.get(pk=pk)
+    except LocalFood.DoesNotExist:
+      raise Http404
+
+  def get(self, request, pk):
+    local_food = self.get_localfood(pk)
+    local_food_serializer = LocalFoodSerializer(local_food)
+    return Response(local_food_serializer.data)
+
+  def patch(self, request, pk):
+    local_food = self.get_localfood(pk)
+    local_food_serializer = LocalFoodSerializer(local_food, data=request.data)
+    if local_food_serializer.is_valid():
+      local_food_serializer.save()
+      return Response(local_food_serializer.data)
+    return Response(local_food_serializer.errors)
+
+  def delete(self, request, pk):
+    local_food = self.get_localfood(pk)
+    local_food.delete()
+    return Response('Eliminado')
--- apps/localfood/api/urls.py
+++ apps/localfood/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .api import LocalFoodAPIView
+from .api import LocalFoodAPIView, LocalFoodDetailAPIView
 
 urlpatterns = [
-  path('', LocalFoodAPIView.as_view(), name='negocios')
+  path('', LocalFoodAPIView.as_view(), name='negocios'),
+  path('<int:pk>/', LocalFoodDetailAPIView.as_view(), name='negocio'),
 ]
