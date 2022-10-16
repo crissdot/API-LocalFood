09c541a1b28db6d4bd12d54412dec187ba3c60eb
crissdot <crissdotdev@gmail.com>
2022-10-16 14:01:19 -0500

    add CRUD operations to users

--- apps/user/api/api.py
+++ apps/user/api/api.py
@@ -12,3 +12,40 @@ def user_api_view(request):
     users = User.objects.all()
     users_serializers = UserSerializer(users, many=True)
     return Response(users_serializers.data)
+
+  elif request.method == 'POST':
+    user_serializer = UserSerializer(data=request.data)
+    if user_serializer.is_valid():
+      user_serializer.save()
+      return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+    return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PATCH', 'DELETE'])
+def user_detail_api_view(request, pk=None):
+
+  if request.method == 'GET':
+    try:
+      user = User.objects.get(pk=pk)
+      user_serializer = UserSerializer(user)
+      return Response(user_serializer.data)
+    except User.DoesNotExist:
+      raise Http404
+
+  elif request.method == 'PATCH':
+    try:
+      user = User.objects.get(pk=pk)
+      user_serializer = UserSerializer(user, data=request.data, partial=True)
+      if user_serializer.is_valid():
+        user_serializer.save()
+        return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+      return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except User.DoesNotExist:
+      raise Http404
+
+  elif request.method == 'DELETE':
+    try:
+      user = User.objects.get(pk=pk)
+      user.delete()
+      return Response({'detail': 'Usuario eliminado correctamente'})
+    except User.DoesNotExist:
+      raise Http404
