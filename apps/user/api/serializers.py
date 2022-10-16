50daee1e7ad1de919a9f9ae7eaecfe93473b3487
crissdot <crissdotdev@gmail.com>
2022-10-16 14:14:04 -0500

    add password encrypt

--- apps/user/api/serializers.py
+++ apps/user/api/serializers.py
@@ -5,3 +5,15 @@ class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
     fields = ('id', 'username', 'password')
+
+  def create(self, validated_data):
+    user = User(**validated_data)
+    user.set_password(validated_data['password'])
+    user.save()
+    return user
+
+  def update(self, instance, validated_data):
+    updated_user = super().update(instance, validated_data)
+    updated_user.set_password(validated_data['password'])
+    updated_user.save()
+    return updated_user
