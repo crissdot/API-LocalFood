5633baeaa553dcb43d4128e051bff7b0892e799b
crissdot <crissdotdev@gmail.com>
2022-10-17 21:03:23 -0500

    add products url

--- apps/products/api/api.py
+++ apps/products/api/api.py
@@ -1,10 +1,17 @@
 from rest_framework import generics
 
 from ..models import Category, Product
-from .serializers import CategorySerializer
+from .serializers import CategorySerializer, ProductSerializer
 
 class CategoryListAPIView(generics.ListAPIView):
   serializer_class = CategorySerializer
 
   def get_queryset(self):
     return Category.objects.filter(state=True)
+
+
+class ProductListAPIView(generics.ListAPIView):
+  serializer_class = ProductSerializer
+
+  def get_queryset(self):
+    return Product.objects.filter(state=True)
--- apps/products/api/urls.py
+++ apps/products/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .api import CategoryListAPIView
+from .api import CategoryListAPIView, ProductListAPIView
 
 urlpatterns = [
+  path('', ProductListAPIView.as_view(), name='productos'),
   path('categorias/', CategoryListAPIView.as_view(), name='categorias'),
 ]
