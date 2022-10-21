e4fe3b0f65526cd0a900a5cb8d31692aff90aa0f
crissdot <crissdotdev@gmail.com>
2022-10-20 19:16:13 -0500

    add show id to categories in addmin

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import Category, Product
 
 # Register your models here.
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+  list_display = ('id', 'description')
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product)
