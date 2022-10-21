926c5e8e73516928ac0d050fbfecae51ec54c2ab
crissdot <crissdotdev@gmail.com>
2022-10-20 19:28:16 -0500

    replace api view with list create api view

--- apps/products/api/api.py
+++ apps/products/api/api.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
 
 from ..models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
@@ -10,8 +12,13 @@ class CategoryListAPIView(generics.ListAPIView):
     return Category.objects.filter(state=True)
 
 
-class ProductListAPIView(generics.ListAPIView):
+class ProductListAPIView(generics.ListCreateAPIView):
   serializer_class = ProductSerializer
+  queryset = ProductSerializer.Meta.model.objects.filter(state = True)
 
-  def get_queryset(self):
-    return Product.objects.filter(state=True)
+  def post(self, request):
+    product_serializer = ProductSerializer(data = request.data)
+    if product_serializer.is_valid():
+      product_serializer.save()
+      return Response(product_serializer.data, status=status.HTTP_201_CREATED)
+    return Response(product_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
