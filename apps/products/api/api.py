12da09a4f2c9392694b465cbede0ad7f054e7103
crissdot <crissdotdev@gmail.com>
2022-10-20 20:21:22 -0500

    add product detail api view

--- apps/products/api/api.py
+++ apps/products/api/api.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
@@ -14,7 +15,7 @@ class CategoryListAPIView(generics.ListAPIView):
 
 class ProductListAPIView(generics.ListCreateAPIView):
   serializer_class = ProductSerializer
-  queryset = ProductSerializer.Meta.model.objects.filter(state = True)
+  queryset = Product.objects.filter(state = True)
 
   def post(self, request):
     product_serializer = ProductSerializer(data = request.data)
@@ -22,3 +23,28 @@ class ProductListAPIView(generics.ListCreateAPIView):
       product_serializer.save()
       return Response(product_serializer.data, status=status.HTTP_201_CREATED)
     return Response(product_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProductDetailApiView(generics.RetrieveUpdateDestroyAPIView):
+  serializer_class = ProductSerializer
+
+  def get_queryset(self, pk=None):
+    if pk is None:
+      return Product.objects.filter(state = True)
+    try:
+      return Product.objects.filter(id=pk, state = True)
+    except Product.DoesNotExist:
+      raise Http404
+
+  def patch(self, request, pk=None):
+    product = self.get_queryset(pk)
+    product_serializer = ProductSerializer(product, data=request.data, partial=True)
+    if product_serializer.is_valid():
+      product_serializer.save()
+      return Response(product_serializer.data)
+    return Response(product_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def delete(self, request, pk=None):
+    product = self.get_queryset(pk)
+    product.state = False
+    product.save()
+    return Response({'detail': 'Producto eliminado correctamente'})
--- apps/products/api/urls.py
+++ apps/products/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .api import CategoryListAPIView, ProductListAPIView
+from .api import CategoryListAPIView, ProductListAPIView, ProductDetailApiView
 
 urlpatterns = [
   path('', ProductListAPIView.as_view(), name='productos'),
+  path('<int:pk>/', ProductDetailApiView.as_view(), name='producto'),
   path('categorias/', CategoryListAPIView.as_view(), name='categorias'),
 ]
