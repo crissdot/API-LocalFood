450b4bf2848a6fbb1af6db7f4ed01bf81e06f03d
crissdot <crissdotdev@gmail.com>
2022-10-22 14:53:01 -0500

    fix error when trying to view an image

--- APILocalFood/urls.py
+++ APILocalFood/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -41,4 +43,4 @@ urlpatterns = [
     path('negocios/', include('apps.localfood.api.urls')),
     path('usuarios/', include('apps.user.api.urls')),
     path('productos/', include('apps.products.api.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
