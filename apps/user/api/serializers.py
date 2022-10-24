eac82159873b1b426b6d8366151a9e1a379c357c
crissdot <crissdotdev@gmail.com>
2022-10-23 21:29:05 -0500

    exclude attrs when retreive a user

--- apps/user/api/serializers.py
+++ apps/user/api/serializers.py
@@ -4,7 +4,7 @@ from ..models import User
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
-    exclude = ('is_active', 'deleted_at', 'modified_at', 'created_at')
+    exclude = ('last_login', 'is_superuser', 'is_active', 'deleted_at', 'modified_at', 'created_at')
 
   def create(self, validated_data):
     user = User(**validated_data)
@@ -17,3 +17,13 @@ class UserSerializer(serializers.ModelSerializer):
     updated_user.set_password(validated_data['password'])
     updated_user.save()
     return updated_user
+
+  def to_representation(self, instance):
+    return {
+      'id': instance.id,
+      'username': instance.username,
+      'name': instance.name,
+      'last_name': instance.last_name,
+      'phone_number': instance.phone_number,
+      'email': instance.email,
+    }
