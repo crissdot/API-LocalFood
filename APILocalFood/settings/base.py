7bb2eabeb283cf9b3f4f7ce89e12d92fcc89af66
crissdot <crissdotdev@gmail.com>
2023-01-10 20:34:59 -0600

    add env variables

--- APILocalFood/settings/base.py
+++ APILocalFood/settings/base.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+import environ
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ha$5se^)-s@9!$+a4a!xink7n(a6#(%ig&t_r8y$@isseds$&l'
+SECRET_KEY = env('SECRET_KEY')
 
 
 # Application definition
@@ -135,3 +139,24 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Temporarily
 CORS_ALLOW_ALL_ORIGINS = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASS'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': ''
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = 'static/'
