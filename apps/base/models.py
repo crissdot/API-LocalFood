9e0b78d03319e27114b6c4b727febf83b0a2a0f4
crissdot <crissdotdev@gmail.com>
2023-01-15 18:16:10 -0600

    delete old migrations

--- apps/base/models.py
+++ apps/base/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from simple_history.models import HistoricalRecords
 
 # Create your models here.
 class BaseModel(models.Model):
@@ -8,14 +7,6 @@ class BaseModel(models.Model):
   created_at = models.DateField(auto_now=False, auto_now_add=True)
   modified_at = models.DateField(auto_now=True, auto_now_add=False)
   deleted_at = models.DateField(auto_now=True, auto_now_add=False)
-  historical = HistoricalRecords(user_model='user.User', inherit=True)
 
-  @property
-  def _history_user(self):
-    return self.changed_by
-
-  @_history_user.setter
-  def _history_user(self, value):
-    self.changed_by = value
   class Meta:
     abstract = True
